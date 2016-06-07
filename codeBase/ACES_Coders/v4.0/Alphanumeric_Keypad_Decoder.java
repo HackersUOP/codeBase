d837a5180cf99ebe3c2c7ea60953317339c3d0aa
samurdhilbk <samurdhilbk@gmail.com>
2016-06-07 22:25:03 +0530

    New Answer

--- codeBase/ACES_Coders/v4.0/Alphanumeric_Keypad_Decoder.java
+++ codeBase/ACES_Coders/v4.0/Alphanumeric_Keypad_Decoder.java
@@ -1,3 +1,16 @@
+/*
+Copyright Hackers' Club, University Of Peradeniya
+Author : E/13/181 (Samurdhi Karunarathne)
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at *
+http://www.apache.org/licenses/LICENSE-2.0 *
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 import java.io.*;
 import java.util.*;
 import java.text.*;
@@ -5,92 +18,90 @@ import java.math.*;
 import java.util.regex.*;
 
 public class Solution {
-        static Map<Character,String> map;
-        static String[] nums;
-        static List<String> words;
+    static Map<Character, String> map;
+    static String[] nums;
+    static List<String> words;
     public static void main(String[] args) {
-        Scanner inp=new Scanner(System.in);
-        words=new ArrayList<String>();
-        String st=inp.next();
-        while(!st.equals("#")){
+        Scanner inp = new Scanner(System.in);
+        words = new ArrayList<String>();
+        String st = inp.next();
+        while (!st.equals("#")) {
             words.add(st);
-            st=inp.next();
+            st = inp.next();
         }
-        String strng=inp.next();
-        strng="@00".concat(strng);
-        String[] arr=strng.split("0+");
-        arr=Arrays.copyOfRange(arr,1,arr.length);
-        nums=new String[words.size()];
-        map=new HashMap<Character,String>();
-        map.put('a',"2");
-        map.put('b',"2");
-        map.put('c',"2");
-        map.put('d',"3");
-        map.put('e',"3");
-        map.put('f',"3");
-        map.put('g',"4");
-        map.put('h',"4");
-        map.put('i',"4");
-        map.put('j',"5");
-        map.put('k',"5");
-        map.put('l',"5");
-        map.put('m',"6");
-        map.put('n',"6");
-        map.put('o',"6");
-        map.put('p',"7");
-        map.put('q',"7");
-        map.put('r',"7");
-        map.put('s',"7");
-        map.put('t',"8");        
-        map.put('u',"8");
-        map.put('v',"8");
-        map.put('w',"9");
-        map.put('x',"9");
-        map.put('y',"9");
-        map.put('z',"9");
-        
-        int l1=words.size();
-        for(int j=0;j<l1;j++){
-            nums[j]=cnums(words.get(j));
+        String strng = inp.next();
+        strng = "@00".concat(strng);
+        String[] arr = strng.split("0+");
+        arr = Arrays.copyOfRange(arr, 1, arr.length);
+        nums = new String[words.size()];
+        map = new HashMap<Character, String>();
+        map.put('a', "2");
+        map.put('b', "2");
+        map.put('c', "2");
+        map.put('d', "3");
+        map.put('e', "3");
+        map.put('f', "3");
+        map.put('g', "4");
+        map.put('h', "4");
+        map.put('i', "4");
+        map.put('j', "5");
+        map.put('k', "5");
+        map.put('l', "5");
+        map.put('m', "6");
+        map.put('n', "6");
+        map.put('o', "6");
+        map.put('p', "7");
+        map.put('q', "7");
+        map.put('r', "7");
+        map.put('s', "7");
+        map.put('t', "8");
+        map.put('u', "8");
+        map.put('v', "8");
+        map.put('w', "9");
+        map.put('x', "9");
+        map.put('y', "9");
+        map.put('z', "9");
+
+        int l1 = words.size();
+        for (int j = 0; j < l1; j++) {
+            nums[j] = cnums(words.get(j));
         }
-        for(int k=0;k<arr.length;k++){
+        for (int k = 0; k < arr.length; k++) {
             System.out.print(gen(arr[k]).concat(" "));
         }
     }
-    public static String cnums(String a){
-        int l=a.length();
-        String ret="";
-        for(int i=0;i<l;i++){
-            Character c1=Character.valueOf(a.charAt(i));
-            ret=ret.concat(map.get(c1));
+    public static String cnums(String a) {
+        int l = a.length();
+        String ret = "";
+        for (int i = 0; i < l; i++) {
+            Character c1 = Character.valueOf(a.charAt(i));
+            ret = ret.concat(map.get(c1));
         }
         return ret;
     }
-    public static String gen(String b){
-        int n=nums.length;
-        List<String> arr=new ArrayList<String>();
-        for(int i=0;i<n;i++){
-            if(nums[i].equals(b)){
+    public static String gen(String b) {
+        int n = nums.length;
+        List<String> arr = new ArrayList<String>();
+        for (int i = 0; i < n; i++) {
+            if (nums[i].equals(b)) {
                 arr.add(words.get(i));
             }
         }
-        int d=arr.size();
-        if(d==1){
+        int d = arr.size();
+        if (d == 1) {
             return arr.get(0);
-        }
-        else if(d==0){
-            String g="";
-            for(int h=0;h<b.length();h++){
-                g=g.concat("*");
+        } else if (d == 0) {
+            String g = "";
+            for (int h = 0; h < b.length(); h++) {
+                g = g.concat("*");
             }
             return g;
-        }
-        else{
-           String r=arr.get(0);
-           for(int j=1;j<d;j++){
-               r=r.concat("|").concat(arr.get(j));
-           } 
-           return "[".concat(r).concat("]");
+        } else {
+            String r = arr.get(0);
+            for (int j = 1; j < d; j++) {
+                r = r.concat("|").concat(arr.get(j));
+            }
+            return "[".concat(r).concat("]");
         }
     }
 }
--- codeBase/ACES_Coders/v4.0/Maximally_Ocurring_Patterns.java
+++ codeBase/ACES_Coders/v4.0/Maximally_Ocurring_Patterns.java
@@ -1,3 +1,17 @@
+/*
+Copyright Hackers' Club, University Of Peradeniya
+Author : E/13/181 (Samurdhi Karunarathne)
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at *
+http://www.apache.org/licenses/LICENSE-2.0 *
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 import java.io.*;
 import java.util.*;
 import java.text.*;
@@ -7,28 +21,28 @@ import java.util.regex.*;
 public class Solution {
 
     public static void main(String[] args) {
-        Scanner inp=new Scanner(System.in);
-        int n=inp.nextInt();
-        String[] store=new String[n];
-        for(int i=0;i<n;i++) store[i]=inp.next();
-        int[] cnt=new int[n];
-        Arrays.fill(cnt,0);
-        String str=inp.next();
-        for(int j=0;j<n;j++){
-            int l1=store[j].length();
-                for(int k=0;k<=(str.length()-l1);k++){
-                    if(str.substring(k,k+l1).equals(store[j])){
-                       cnt[j]=cnt[j]+1; 
-                    }
+        Scanner inp = new Scanner(System.in);
+        int n = inp.nextInt();
+        String[] store = new String[n];
+        for (int i = 0; i < n; i++) store[i] = inp.next();
+        int[] cnt = new int[n];
+        Arrays.fill(cnt, 0);
+        String str = inp.next();
+        for (int j = 0; j < n; j++) {
+            int l1 = store[j].length();
+            for (int k = 0; k <= (str.length() - l1); k++) {
+                if (str.substring(k, k + l1).equals(store[j])) {
+                    cnt[j] = cnt[j] + 1;
                 }
+            }
         }
-        int y=0;
-        for(int m=0;m<n;m++){
-            y=Math.max(y,cnt[m]);
+        int y = 0;
+        for (int m = 0; m < n; m++) {
+            y = Math.max(y, cnt[m]);
         }
         System.out.println(y);
-        for(int h=0;h<n;h++){
-            if(cnt[h]==y) System.out.println(store[h]);
+        for (int h = 0; h < n; h++) {
+            if (cnt[h] == y) System.out.println(store[h]);
         }
     }
 }
