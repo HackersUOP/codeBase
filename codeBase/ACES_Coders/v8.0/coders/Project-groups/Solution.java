8a8048aa96e310bbba097ae518d0d9ac6c71528f
Gihan Chanaka Jayatilaka <gihanchanaka@gmail.com>
2020-03-10 14:48:45 +0530

    Create Solution.java

---
+++ codeBase/ACES_Coders/v8.0/coders/Project-groups/Solution.java
@@ -0,0 +1,149 @@
+/*
+    Problem setter's code.
+    gihanjayatilaka[at]eng[dot]pdn[dot]ac[dot]lk
+    2020-02-27
+    
+    The time complexity is O(N^2 log(N))
+    The memory complexity is O(N)
+*/
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Solution {
+    static boolean DEBUG=false;
+    static Scanner sc=new Scanner(System.in);
+    public static void main(String[] args) {
+        String[] temp=sc.nextLine().trim().split(" ");
+
+        int N=Integer.parseInt(temp[0]);
+        int D=Integer.parseInt(temp[1]);
+        float DD=(float)D;
+
+        float[][] theta12=new float[N][2];
+        double[][] xyf=new double[N][2];
+
+
+
+
+        for(int n=0;n<N;n++){
+            temp=sc.nextLine().trim().split(" ");
+            int id=Integer.parseInt(temp[0].split("/")[2])-1;
+
+            theta12[id][0]=Float.parseFloat(temp[1]);
+        }
+
+
+        for(int n=0;n<N;n++){
+            temp=sc.nextLine().trim().split(" ");
+            int id=Integer.parseInt(temp[0].split("/")[2])-1;
+            theta12[id][1]=Float.parseFloat(temp[1]);
+        }
+
+
+        if(DEBUG){
+            for(int n=0;n<N;n++){
+                System.out.println(n+"Angles "+theta12[n][0]+" "+theta12[n][1]);
+            }
+        }
+
+
+
+        for(int n=0;n<N;n++){
+            double cotTheta1=1.0/Math.tan(theta12[n][0]);
+            double t=cotTheta1/Math.tan(Math.PI/2.0 + theta12[n][1]);
+
+            if(DEBUG)System.out.println(n+ " "+cotTheta1+ " "+t);
+            xyf[n][0]=DD/(t+1.0);
+            xyf[n][1]=cotTheta1*xyf[n][0];
+        }
+
+        int[][] xy=new int[N][2];
+
+        for (int n=0;n<N;n++){
+            xy[n][0]=(int)xyf[n][0];
+            xy[n][1]=(int)xyf[n][1];
+        }
+
+        System.out.println(solve(xy));
+
+
+
+    }
+
+    public static long solve(int[][] xy){
+        int N=xy.length;
+        if(DEBUG){
+            for(int n=0;n<N;n++){
+                System.out.println(xy[n][0]+" " +xy[n][1]);
+            }
+        }
+
+        int[] ar=new int[N-1];
+        long ans=0;
+
+        for(int s=0;s<N;s++){
+            if(DEBUG)System.out.print("NUMBER = "+s);
+            int x=xy[s][0];
+            int y=xy[s][1];
+            if(DEBUG)System.out.println(" x,y: "+x+" "+y);
+            int idx=0;
+            for(int ss=0;ss<N;ss++){
+                if(ss!=s) {
+                    ar[idx] = (xy[ss][0] - x) * (xy[ss][0] - x) + (xy[ss][1] - y) * (xy[ss][1] - y);
+                    idx++;
+                }
+            }
+
+            Arrays.sort(ar);
+
+            if(DEBUG){
+                System.out.print("Sorted dist^2: ");
+                for(int k=0;k<ar.length;k++){
+                    System.out.print(ar[k]+" ");
+                }
+                System.out.println();
+            }
+
+
+
+            int curr=ar[0];
+            int currCount=1;
+
+            for(int ss=1;ss<ar.length;ss++){
+                if(DEBUG){
+                    System.out.println(curr+" "+ar[ss]);
+                }
+                if(ar[ss]==curr){
+                    currCount++;
+                    if(ss==ar.length-1){
+                        ans+=(currCount*(currCount-1))/2;
+                        if(DEBUG){
+                            System.out.println("FOUND at end!"+ ans);
+                        }
+                    }
+                }
+                else{
+                    if(currCount>=2){
+                        ans+=(currCount*(currCount-1))/2;
+                        if(DEBUG){
+                            System.out.println("FOUND!"+ ans);
+                        }
+                    }
+                    currCount=1;
+                    curr=ar[ss];
+                }
+
+                if(DEBUG){
+                    System.out.println(curr+" "+ar[ss]+" "+currCount);
+                }
+            }
+        }
+        if(DEBUG){
+            System.out.println("ANSWER: "+ans);
+        }
+
+        return ans;
+    }
+}
