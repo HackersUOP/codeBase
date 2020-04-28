59ed8b640af2206d56c4536f2b8b874b5a63acd2
Sathira Silva <SathiraSofte@gmail.com>
2020-04-28 20:39:17 +0530

    Update Solution.cpp

--- codeBase/HackerRank/Algorithms/Game Theory/Deforestation/Solution.cpp
+++ codeBase/HackerRank/Algorithms/Game Theory/Deforestation/Solution.cpp
@@ -6,22 +6,22 @@
 
 using namespace std;
 
-int gr[501];
+int grundy[501];
 
 int dfs(int u, int p, vector<vector<int>>& G){
     for(auto ch : G[u]){
-        if(ch != p) gr[u] ^= dfs(ch,u,G);
+        if(ch != p) grundy[u] ^= dfs(ch,u,G);
     }
-    return u == 1 ? gr[u] : ++gr[u];
+    return u == 1 ? grundy[u] : ++grundy[u];
 }
 
 
 string deforestation(int n, vector<vector<int>> tree) {
     memset(gr, 0, sizeof(gr));
-    vector<vector<int>> G(n+1);
+    vector<vector<int>> graph(n+1);
     for(auto p : tree){
-        G[p[0]].push_back(p[1]);
-        G[p[1]].push_back(p[0]);
+        graph[p[0]].push_back(p[1]);
+        graph[p[1]].push_back(p[0]);
     }
-    return dfs(1,-1,G) ? "Alice" : "Bob";
+    return dfs(1, -1, graph) ? "Alice" : "Bob";
 }
