6c40e4632bfdbdf0c447e167c8127cb5e01682c9
Sathira Silva <SathiraSofte@gmail.com>
2020-10-08 12:52:50 +0530

    Create solution.cpp

---
+++ codeBase/moraXtreme Past Problems/moraXtreme1.0/Mission of Agent X/solution.cpp
@@ -0,0 +1,22 @@
+/*
+    Copyright (C) 2020, Sathira Silva.
+    
+    Link to the problem: https://www.hackerrank.com/contests/moraxtreme1/challenges/single-input
+    
+    Approach: This is an easy math problem. Find the roots of the given equation. It's obvious that they're complex hence conjugates which implies that when the given expression
+              is evaluated, the result is real for all n. The proof is added to the same folder. See 
+              The final answer for p is: 4cos(npi/3)
+              NOTE: if n is divisible by 3 and n / 3 is odd cos(npi/3) = -1, if n / 3 is even cos(npi/3) = 1; If n is not divisible by 3 but odd, cos(npi/3) = 1/2 and -1/2
+              if n is even otherwise.
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;
+    cout << (n % 3 == 0 ? ((n / 3) & 1 ? -4 : 4) : (n & 1 ? 2 : -2));
+    return 0;
+}
