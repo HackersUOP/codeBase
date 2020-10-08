1ae7a9bf6b39957ded045868137f1b15aa3f3cc5
Sathira Silva <SathiraSofte@gmail.com>
2020-10-08 13:47:52 +0530

    Update solution.cpp

--- codeBase/moraXtreme Past Problems/moraXtreme1.0/Complex Problem/solution.cpp
+++ codeBase/moraXtreme Past Problems/moraXtreme1.0/Complex Problem/solution.cpp
@@ -8,6 +8,8 @@
               The final answer for p is: 4cos(npi/3)
               NOTE: if n is divisible by 3 and n / 3 is odd cos(npi/3) = -1, if n / 3 is even cos(npi/3) = 1; If n is not divisible by 3 but odd, cos(npi/3) = 1/2 and -1/2
               if n is even otherwise.
+              
+    >>> The 6th test case is wrong. For n = 900000000000000000, the result should be p = 4 because cos(900000000000000000pi/3) = 1. But -4 has been put as the expected output.
 */
 
 #include <bits/stdc++.h>
